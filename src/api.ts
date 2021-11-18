5966569c1358e81580b4d56e0f73629f87615245
Frank Kulak <fs.kulak@gmail.com>
2021-11-18 17:50:22 -0500

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,16 @@
+import DBPF from "./models/DBPF";
+import RawResource from "./models/resources/Raw";
+import TuningResource from "./models/resources/Tuning";
+import SimDataResource from "./models/resources/SimData";
+import StringTableResource from "./models/resources/StringTable";
+import UnsupportedResource from "./models/resources/Unsupported";
+
+
 export default {
-  // TODO:
+  DBPF,
+  RawResource,
+  TuningResource,
+  SimDataResource,
+  StringTableResource,
+  UnsupportedResource,
 }
