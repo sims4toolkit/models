5018930ce39912b466325cd4284505d5e589e89d
Frank Kulak <fs.kulak@gmail.com>
2021-11-18 17:34:18 -0500

    Add plainText function to raw resources.

--- src/models/resources/Raw.ts
+++ src/models/resources/Raw.ts
@@ -7,18 +7,34 @@ import type { ResourceVariant } from './Resource';
  */
 export default class RawResource extends Resource {
   readonly variant: ResourceVariant = 'RAW';
+  private _encoding: BufferEncoding;
+  private _content?: string;
 
-  private constructor(buffer: Buffer) {
+  private constructor(buffer: Buffer, encoding: BufferEncoding) {
     super(buffer);
+    this._encoding = encoding;
   }
 
   /**
-   * Creates a new raw resource from the given buffer.
+   * Returns this resource in plain text form. The encoding to use was set when
+   * this resource was created.
+   */
+  plainText(): string {
+    if (this._content === undefined)
+      this._content = this.getBuffer().toString(this._encoding);
+    return this._content;
+  }
+
+  /**
+   * Creates a new raw resource from the given buffer, and reads it in the given
+   * encoding (UTF-8 if not provided). Reading the buffer is done lazily, it
+   * will not actually be decoded until `plainText()` is called.
    * 
    * @param buffer Buffer to create a raw resource from
+   * @param encoding How the buffer is encoded
    */
-  static from(buffer: Buffer): RawResource {
-    return new RawResource(buffer);
+  static from(buffer: Buffer, encoding: BufferEncoding = 'utf-8'): RawResource {
+    return new RawResource(buffer, encoding);
   }
 
   protected _serialize(): Buffer {
