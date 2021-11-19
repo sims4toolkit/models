639cf89e214a216a23367ea032f478e45d55770b
Frank Kulak <fs.kulak@gmail.com>
2021-11-19 16:48:41 -0500

    Update StringTable.test.js

--- test/models/resources/StringTable.test.js
+++ test/models/resources/StringTable.test.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 const expect = require('chai').expect;
 const { StringTableResource } = require('../../../dst/api');
 
-const path = require('path').resolve(__dirname, '../../data/stbls/SmallSTBL.stbl');
-const buffer = fs.readFileSync(path);
+function getSTBL(filename) {
+  const filepath = path.resolve(__dirname, `../../data/stbls/${filename}`);
+  const buffer = fs.readFileSync(filepath);
+  return StringTableResource.from(buffer);
+}
 
 function assertEntry(entry, id, key, string) {
   expect(entry.id).to.equal(id);
@@ -13,14 +17,8 @@ function assertEntry(entry, id, key, string) {
 
 describe('StringTableResource', function() {
   describe('#from()', function() {
-    /** @type {StringTableResource} */
-    let stbl;
-
-    this.beforeEach(function() {
-      stbl = StringTableResource.from(buffer);
-    });
-
     it('should load the contents correctly', function() {
+      const stbl = getSTBL('SmallSTBL.stbl');
       const entries = stbl.getEntries();
       expect(entries).to.be.an('Array');
       expect(entries).to.have.length(3);
@@ -28,5 +26,7 @@ describe('StringTableResource', function() {
       assertEntry(entries[1], 1, 0xF098F4B5, 'This is another string!');
       assertEntry(entries[2], 2, 0x8D6D117D, 'And this, this is a third.');
     });
+
+    // TODO: test for failure
   });
 });
