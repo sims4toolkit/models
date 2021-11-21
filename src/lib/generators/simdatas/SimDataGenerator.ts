7958e52686b61bbce787ddc6dd0e304c49006d03
Frank Kulak <fs.kulak@gmail.com>
2021-11-21 14:48:57 -0500

    Add stub for SimData generator.

---
+++ src/lib/generators/simdatas/SimDataGenerator.ts
@@ -0,0 +1,14 @@
+import * as Hashing from '../../services/Hashing';
+import SimData from '../../models/resources/SimData';
+import type Tuning from '../../models/resources/Tuning';
+
+/**
+ * TODO:
+ * 
+ * @param tuning TODO:
+ * @returns TODO:
+ */
+export function generate(tuning: Tuning): SimData {
+  // TODO: impl
+  return undefined;
+}
