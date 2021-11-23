1464bcd63f11351d27693680d4343d2be74352c8
Frank Kulak <fs.kulak@gmail.com>
2021-11-22 23:12:58 -0500

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,20 @@
-import DBPF from "./lib/models/dbpf";
+import Dbpf from "./lib/models/dbpf";
 import RawResource from "./lib/models/resources/raw";
 import TuningResource from "./lib/models/resources/tuning";
 import SimDataResource from "./lib/models/resources/simdata";
 import StringTableResource from "./lib/models/resources/stringtable";
 import UnsupportedResource from "./lib/models/resources/unsupported";
+import * as tunables from "./lib/models/tunables";
 import * as hashing from "./lib/utils/hashing";
 
 
 export {
-  DBPF,
+  Dbpf,
   RawResource,
   TuningResource,
   SimDataResource,
   StringTableResource,
   UnsupportedResource,
+  tunables,
   hashing
 }
