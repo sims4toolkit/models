5967d3448c27b3dd51c443e5029620a68a498ed0
Frank Kulak <fs.kulak@gmail.com>
2021-12-07 00:02:15 -0500

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import TuningResource from "./lib/models/resources/tuning";
 import SimDataResource from "./lib/models/resources/simdata";
 import StringTableResource from "./lib/models/resources/stringtable";
 import * as nodes from "./lib/models/dom/nodes";
-import * as tunables from "./lib/models/tunables"; // FIXME: delete
+import * as tunables from "./lib/models/dom/tunables";
 import * as hashing from "./lib/utils/hashing";
 import * as formatting from "./lib/utils/formatting";
 
