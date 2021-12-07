b994f0189d14211d79168db939c28183837f81bd
Frank Kulak <fs.kulak@gmail.com>
2021-12-07 00:14:29 -0500

    Update tunables.test.js

---
+++ test/models/dom/tunables.test.js
@@ -0,0 +1,69 @@
+const { expect } = require("chai");
+const { tunables } = require("../../../dst/api");
+const { I, M, T, E, L, U, V, C, Comment, LocString, getLocStringFn } = tunables;
+
+describe("#I()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#M()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#T()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#E()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#L()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#U()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#V()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#C()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#Comment()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#LocString()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
+
+describe("#getLocString()", function() {
+  it("should ...", function() {
+    expect(true).to.be.false; // TODO:
+  });
+});
