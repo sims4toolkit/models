1f05801470b83f5ebe25a84e4099d05081aa3393
Frank Kulak <fs.kulak@gmail.com>
2021-12-29 21:58:52 -0500

    Update simDataResource.ts

--- src/lib/models/resources/simData/simDataResource.ts
+++ src/lib/models/resources/simData/simDataResource.ts
@@ -8,34 +8,35 @@ import { SimDataInstance, SimDataSchema } from "./fragments";
  * A resource for SimData (binary tuning). SimDatas are essentially mini
  * relational databases, but to simplify working with them (and for consistency
  * with Sims 4 Studio), this model uses the concept of "instances". An
- * "instance" is an object value that has a name.
+ * "instance" is an object cell that has a name.
  */
 export default class SimDataResource extends Resource {
   readonly variant = 'DATA';
-
-  private _schemas: SimDataSchema[];
-  /** The schemas in this SimData. */
-  get schemas() { return this._schemas; }
-  set schemas(schemas) {
-    schemas.forEach(schema => schema.owner = this);
-    this._schemas = schemas;
-    this.uncache();
-  }
   
+  private _schemas: SimDataSchema[];
   private _instances: SimDataInstance[];
-  /** The instance in this SimData. */
-  get instances() { return this._instances; }
-  set instances(instances) {
-    instances.forEach(instance => instance.owner = this);
-    this._instances = instances;
-    this.uncache();
-  }
 
-  protected constructor({ schemas = [], instances = [], buffer }: {
-    schemas?: SimDataSchema[];
-    instances?: SimDataInstance[];
-    buffer?: Buffer;
-  } = {}) {
+  /**
+   * The schemas in this SimData. Individual schemas can be mutated and cacheing
+   * will be handled (e.g. `schemas[0].name = "Schema"` is perfectly safe),
+   * however, mutating the array itself by adding or removing schemas should be
+   * avoided whenever possible, because doing so is a surefire way to mess up
+   * the cache. 
+   * 
+   * To add schemas, use the `addSchemas()` or `addSchemaClones()` methods. To
+   * remove schemas, use the `removeSchemas()` method or call `delete()` on
+   * individual schemas.
+   * 
+   * If you insist on removing from or sorting the array manually, you can, as
+   * long as you remember to call `uncache()` when you are done. If you insist
+   * on adding columns manually, it's your funeral.
+   */
+  get schemas() { return this._schemas; }
+  
+  /** The instances (named objects) in this SimData. */
+  get instances() { return this._instances; }
+
+  protected constructor(schemas: SimDataSchema[], instances: SimDataInstance[], buffer?: Buffer) {
     super({ buffer });
     schemas.forEach(schema => schema.owner = this);
     this._schemas = schemas;
