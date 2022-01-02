37e1077c773898388271cf7b721793c7a710cc30
Frank Kulak <fs.kulak@gmail.com>
2022-01-02 00:22:01 -0500

    Finish first draft of simdata writing.

--- src/lib/models/resources/simData/serialization/writeData.ts
+++ src/lib/models/resources/simData/serialization/writeData.ts
@@ -1,7 +1,7 @@
 import { BinaryEncoder } from "../../../../utils/encoding";
 import { formatAsHexString } from "../../../../utils/formatting";
 import { fnv32 } from "../../../../utils/hashing";
-import { SimDataDto, HEADER_SIZE, TABLE_HEADER_OFFSET, RELOFFSET_NULL, NO_NAME_HASH, SUPPORTED_VERSION } from "../shared";
+import { SimDataDto, HEADER_SIZE, TABLE_HEADER_OFFSET, RELOFFSET_NULL, NO_NAME_HASH, SUPPORTED_VERSION, CellEncodingOptions } from "../shared";
 import { SimDataType, SimDataTypeUtils } from "../simDataTypes";
 import * as cells from "../simDataCells";
 
@@ -34,7 +34,7 @@ interface RawTable {
 /** A table in a DATA file that contains objects that follow a schema. */
 interface ObjectTable {
   name?: string; // iff it's a table for an instance
-  schemaHash: number;
+  schema: SerialSchema;
   rows: TableRow[];
 }
 
@@ -108,7 +108,7 @@ export default function writeData(model: SimDataDto): Buffer {
   const objectTables: { [key: number]: ObjectTable; } = {};
   function getObjectTable(schemaHash: number): ObjectTable {
     if (!(schemaHash in objectTables)) objectTables[schemaHash] = {
-      schemaHash,
+      schema: serialSchemaMap[schemaHash],
       rows: []
     };
 
@@ -201,7 +201,7 @@ export default function writeData(model: SimDataDto): Buffer {
       index: charTableLength
     };
 
-    charTableLength += Buffer.byteLength(cell.value) + 1; // +1 for null
+    charTableLength += Buffer.byteLength(cell.value, 'utf-8') + 1; // +1 for null
     stringsToAddToCharTable.push(cell.value);
 
     return ref;
@@ -257,14 +257,14 @@ export default function writeData(model: SimDataDto): Buffer {
     const strings = Object.keys(nameHashes);
 
     const buffer = Buffer.alloc(strings.reduce((prev, string) => {
-      return prev + Buffer.byteLength(string) + 1;
+      return prev + Buffer.byteLength(string, 'utf-8') + 1;
     }, 0));
   
     let offset = 0;
     strings.forEach(string => {
       buffer.write(string, offset, 'utf-8');
       stringTableOffsets[string] = offset;
-      offset += Buffer.byteLength(string) + 1;
+      offset += Buffer.byteLength(string, 'utf-8') + 1;
     });
   
     return buffer;
@@ -322,37 +322,34 @@ export default function writeData(model: SimDataDto): Buffer {
   // 3 sections combined to make alignment easier
   const headerAndTablesBuffer: Buffer = ((): Buffer => {
     let totalSize = HEADER_SIZE;
-    const hasCharTable = charTableEntries.length > 0;
-    const numTables = objectTables.length + valueTables.length + (hasCharTable ? 1 : 0);
+    const hasCharTable = stringsToAddToCharTable.length > 0;
+    const numTables = Object.keys(rawTables).length + Object.keys(objectTables).length + (hasCharTable ? 1 : 0);
     totalSize += numTables * 28;
 
-    const objectTablePositions: { [key: number]: number; } = {}; // use schema hash
-    objectTables.forEach(table => {
+    /** Maps schema hash to position of object table. */
+    const objectTablePositions: { [key: number]: number; } = {};
+    for (const schemaHash in objectTables) {
+      const table = objectTables[schemaHash];
       totalSize += getPaddingForAlignment(totalSize, 15);
       totalSize += getPaddingForAlignment(totalSize, table.schema.size - 1);
       objectTablePositions[table.schema.hash] = totalSize;
-      totalSize += table.schema.size * table.objects.length;
-    });
+      totalSize += table.schema.size * table.rows.length; // FIXME: is rows the same as objects in old code?
+    }
 
-    const valueTablePositions: { [key: number]: number; } = {}; // use data type
-    valueTables.forEach(table => {
+    /** Maps data type to position of raw table. */
+    const rawTablePositions: { [key: number]: number; } = {};
+    for (const dataType in rawTables) {
+      const table = rawTables[dataType];
       totalSize += getPaddingForAlignment(totalSize, 15);
-      totalSize += getPaddingForAlignment(totalSize, getAlignmentForType(table.dataType) - 1);
-      valueTablePositions[table.dataType] = totalSize;
-      totalSize += getBytesForType(table.dataType) * table.values.length;
-    });
+      totalSize += getPaddingForAlignment(totalSize, SimDataTypeUtils.getAlignment(table.dataType) - 1);
+      rawTablePositions[table.dataType] = totalSize;
+      totalSize += SimDataTypeUtils.getBytes(table.dataType) * table.row.length; // FIXME: is row the same as values in old code?
+    }
 
-    const stringPositions: { [key: number]: number; } = {}; // use string index
-    let charTableSize = 0;
     if (hasCharTable) {
       totalSize += getPaddingForAlignment(totalSize, 15);
-      valueTablePositions[SimDataType.Character] = totalSize;
-      charTableEntries.forEach((string, i) => {
-        stringPositions[i] = totalSize;
-        const stringSize = Buffer.byteLength(string, 'utf-8') + 1;
-        charTableSize += stringSize;
-        totalSize += stringSize;
-      });
+      rawTablePositions[SimDataType.Character] = totalSize;
+      totalSize += charTableLength;
     }
 
     totalSize += getPaddingForAlignment(totalSize, 15);
@@ -372,61 +369,34 @@ export default function writeData(model: SimDataDto): Buffer {
     // table header and data
     const bytesLeft = () => totalSize - encoder.tell();
 
-    function getRelativeStringOffset(stringIndex: number): number {
-      return stringPositions[stringIndex] - encoder.tell();
-    }
+    function getOffsetForTableRef(ref: TableRef): number {
+      if (ref.dataType === undefined) return RELOFFSET_NULL;
 
-    function getRelativeObjectOffset(objectValue: { schemaHash?: number; index: number; }): number {
-      const schema = schemaFromHash[objectValue.schemaHash];
-      const tablePos = objectTablePositions[objectValue.schemaHash];
-      return tablePos + (schema.size * objectValue.index) - encoder.tell();
-    }
-
-    function getRelativeDataRefOffset(ref: DataRef): number {
+      let pos: number, offset: number;
       if (ref.dataType === SimDataType.Object) {
-        return getRelativeObjectOffset(ref);
+        pos = objectTablePositions[ref.schemaHash];
+        offset = serialSchemaMap[ref.schemaHash].size * ref.index;
       } else {
-        const tablePos = valueTablePositions[ref.dataType];
-        const offset = getBytesForType(ref.dataType) * ref.index;
-        return tablePos + offset - encoder.tell();
+        pos = rawTablePositions[ref.dataType];
+        offset = SimDataTypeUtils.getBytes(ref.dataType) * ref.index;
       }
+
+      return pos + offset - encoder.tell();
     }
 
-    function writeValue(dataType: SimDataType, value: any) {
-      switch (dataType) {
+    function writeCell(tableCell: TableCell) {
+      const options: CellEncodingOptions = {};
 
-        case SimDataType.String:
-          encoder.uint32(getRelativeStringOffset(value.stringIndex));
-          return;
-        case SimDataType.HashedString:
-          encoder.uint32(getRelativeStringOffset(value.stringIndex));
-          encoder.uint32(value.stringHash);
-          return;
-        case SimDataType.Object:
-          encoder.uint32(getRelativeObjectOffset(value));
-          return;
-        case SimDataType.Vector:
-          if (value.count === 0) {
-            encoder.int32(RELOFFSET_NULL); // intentionally signed
-          } else {
-            encoder.uint32(getRelativeDataRefOffset(value.ref));
-          }
-          encoder.uint32(value.count);
-          return;
-        case SimDataType.Variant:
-          if (value.ref === undefined) {
-            encoder.int32(RELOFFSET_NULL);
-          } else {
-            encoder.int32(getRelativeDataRefOffset(value.ref)); // intentionally signed
-          }
-          encoder.uint32(value.typeHash);
-          return;
-        default:
-          throw new Error(`Data type ${dataType} is not recognized (value = ${value}).`);
+      if (tableCell.ref) {
+        options.offset = getOffsetForTableRef(tableCell.ref);
       }
+
+      tableCell.cell.encode(encoder, options);
     }
 
-    objectTables.forEach(table => {
+    for (const schemaHash in objectTables) {
+      const table = objectTables[schemaHash];
+
       // header
       if (table.name === undefined) {
         encoder.int32(RELOFFSET_NULL);
@@ -440,41 +410,36 @@ export default function writeData(model: SimDataDto): Buffer {
       encoder.uint32(table.schema.size);
       const tablePos = objectTablePositions[table.schema.hash];
       encoder.int32(tablePos - encoder.tell());
-      encoder.uint32(table.objects.length);
+      encoder.uint32(table.rows.length); // FIXME: rows == objects?
 
       // data
       encoder.savePos(() => {
         encoder.seek(tablePos);
-        table.objects.forEach(obj => {
-          table.schema.columns.forEach(column => {
-            encoder.savePos(() => {
-              encoder.skip(column.offset);
-              writeValue(column.dataType, obj[column.name]);
-            });
-          });
+        table.rows.forEach(row => {
+          row.forEach(writeCell);
         });
       });
-    });
+    }
 
-    valueTables.forEach(table => {
+    for (const dataType in rawTables) {
+      const table = rawTables[dataType];
+
       // header
       encoder.int32(RELOFFSET_NULL);
       encoder.uint32(NO_NAME_HASH);
       encoder.int32(RELOFFSET_NULL);
       encoder.uint32(table.dataType);
-      encoder.uint32(getBytesForType(table.dataType));
-      const tableOffset = valueTablePositions[table.dataType];
+      encoder.uint32(SimDataTypeUtils.getBytes(table.dataType));
+      const tableOffset = rawTablePositions[table.dataType];
       encoder.int32(tableOffset - encoder.tell());
-      encoder.uint32(table.values.length);
+      encoder.uint32(table.row.length); // FIXME: row == values?
 
       // data
       encoder.savePos(() => {
         encoder.seek(tableOffset);
-        table.values.forEach(value => {
-          writeValue(table.dataType, value);
-        });
+        table.row.forEach(writeCell);
       });
-    });
+    }
 
     if (hasCharTable) {
       // header
@@ -483,14 +448,14 @@ export default function writeData(model: SimDataDto): Buffer {
       encoder.int32(RELOFFSET_NULL);
       encoder.uint32(SimDataType.Character);
       encoder.uint32(1); // bytes for a char
-      const tableOffset = valueTablePositions[SimDataType.Character];
+      const tableOffset = rawTablePositions[SimDataType.Character];
       encoder.int32(tableOffset - encoder.tell());
-      encoder.uint32(charTableSize);
+      encoder.uint32(charTableLength);
 
       // data
       encoder.savePos(() => {
         encoder.seek(tableOffset);
-        charTableEntries.forEach(string => {
+        stringsToAddToCharTable.forEach(string => {
           encoder.charsUtf8(string);
           encoder.skip(1);
         });
