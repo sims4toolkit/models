09d658b724d71f593be950f0a72a1392f7373d52
Frank Kulak <fs.kulak@gmail.com>
2022-01-02 17:48:35 -0500

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import Dbpf from "./lib/models/dbpf";
+import Dbpf from "./lib/models/dbpf/dbpf";
 import XmlResource from "./lib/models/resources/generic/xmlResource";
 import RawResource from "./lib/models/resources/generic/rawResource";
 import TuningResource from "./lib/models/resources/tuning/tuningResource";
@@ -8,6 +8,9 @@ import * as xmlDom from "./lib/models/xml/dom";
 import * as tunables from "./lib/models/resources/tuning/tunables";
 import * as hashing from "./lib/utils/hashing";
 import * as formatting from "./lib/utils/formatting";
+import * as simDataCells from "./lib/models/resources/simData/simDataCells";
+import * as simDataFragments from "./lib/models/resources/simData/simDataFragments";
+import * as simDataTypes from "./lib/models/resources/simData/simDataTypes";
 
 
 export {
@@ -18,6 +21,10 @@ export {
   TuningResource,
   SimDataResource,
   StringTableResource,
+  // SimData
+  simDataCells,
+  simDataFragments,
+  simDataTypes,
   // Modules
   xmlDom,
   tunables,
