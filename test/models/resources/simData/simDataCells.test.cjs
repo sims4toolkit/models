7bd788592fc5047b11361e836fb67881c0d891b2
Frank Kulak <fs.kulak@gmail.com>
2022-01-03 12:47:05 -0500

    Stubs for cell tests.

--- test/models/resources/simData/simDataCells.test.cjs
+++ test/models/resources/simData/simDataCells.test.cjs
@@ -1,6 +1,18 @@
 const { expect } = require("chai");
 const cells = require("../../../../dst/api").simDataCells;
 
-describe("", function(arguments) {
+describe("Cell", function() {
+  describe("#parseXmlNode()", function() {
+    it("should be implemented", () => {
+      expect(true).to.be.false; // TODO:
+    });
+  });
+});
+
+describe("BooleanCell", function() {
   // body
+});
+
+describe('', () => {
+  
 });
