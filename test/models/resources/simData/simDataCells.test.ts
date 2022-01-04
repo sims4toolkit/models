c92225adfb17c3e7c309104a8bd26b411c3c9d50
Frank Kulak <fs.kulak@gmail.com>
2022-01-03 22:57:40 -0500

    Add test stubs for rest of primitive cells.

--- test/models/resources/simData/simDataCells.test.ts
+++ test/models/resources/simData/simDataCells.test.ts
@@ -1,18 +1,34 @@
 import { expect } from "chai";
 import { simDataCells } from "../../../../dst/api";
-import { BinaryEncoder } from "../../../../dst/lib/utils/encoding";
+import { BinaryDecoder, BinaryEncoder } from "../../../../dst/lib/utils/encoding";
 
 const cells = simDataCells;
 
 describe("Cell", function() {
   describe("static#parseXmlNode()", function() {
-    it("should be implemented", () => {
-      expect(true).to.be.false; // TODO:
-    });
+    // TODO:
   });
 });
 
 describe("BooleanCell", function() {
+  describe("#constructor", () => {
+    it("should use the value that is given", () => {
+      const cell = new cells.BooleanCell(true);
+      expect(cell.value).to.be.true;
+    });
+
+    it("should have an undefined owner is none is given", () => {
+      const cell = new cells.BooleanCell(true);
+      expect(cell.owner).to.be.undefined;
+    });
+
+    it("should have an owner if one is given", () => {
+      const owner = cells.VariantCell.getDefault();
+      const cell = new cells.BooleanCell(true, owner);
+      expect(cell.owner).to.equal(owner);
+    });
+  });
+
   describe("#clone()", () => {
     it("should not mutate the original", () => {
       const cell = new cells.BooleanCell(true);
@@ -69,7 +85,74 @@ describe("BooleanCell", function() {
   });
 
   describe("static#decode()", () => {
+    it("should read 1 as true", () => {
+      const buffer = Buffer.alloc(1);
+      const encoder = new BinaryEncoder(buffer);
+      encoder.uint8(1);
+      const decoder = new BinaryDecoder(buffer);
+      const cell = cells.BooleanCell.decode(decoder);
+      expect(cell.value).to.be.true;
+    });
+
+    it("should read 0 as false", () => {
+      const buffer = Buffer.alloc(1);
+      const decoder = new BinaryDecoder(buffer);
+      const cell = cells.BooleanCell.decode(decoder);
+      expect(cell.value).to.be.false;
+    });
+  });
+
+  describe("static#fromXmlNode()", () => {
     // TODO:
+  });
+
+  describe("static#getDefault()", () => {
+    it("should create a cell with a false value", () => {
+      const cell = cells.BooleanCell.getDefault();
+      expect(cell.value).to.be.false;
+    });
+  });
+});
+
+describe("TextCell", function() {
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#encode()", () => {
+    context("data type === character", () => {
+      // TODO:
+    });
+
+    context("data type === string", () => {
+      // TODO:
+    });
+
+    context("data type === hashed string", () => {
+      // TODO:
+    });
+  });
+
+  describe("#toXmlNode()", () => {
+    // TODO:
+  });
+
+  describe("#validate()", () => {
+    // TODO:
+  });
+
+  describe("static#decode()", () => {
+    context("data type === character", () => {
+      // TODO:
+    });
+
+    context("data type === string", () => {
+      // TODO:
+    });
+
+    context("data type === hashed string", () => {
+      // TODO:
+    });
   });
 
   describe("static#fromXmlNode()", () => {
@@ -81,7 +164,67 @@ describe("BooleanCell", function() {
   });
 });
 
-describe("TextCell", function() {
+describe("NumberCell", function() {
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#encode()", () => {
+    context("data type === 8 bits", () => {
+      // TODO:
+    });
+
+    context("data type === 16 bits", () => {
+      // TODO:
+    });
+
+    context("data type === 36 bits", () => {
+      // TODO:
+    });
+  });
+
+  describe("#toXmlNode()", () => {
+    // TODO:
+  });
+
+  describe("#validate()", () => {
+    context("data type === 8 bits", () => {
+      // TODO:
+    });
+
+    context("data type === 16 bits", () => {
+      // TODO:
+    });
+
+    context("data type === 36 bits", () => {
+      // TODO:
+    });
+  });
+
+  describe("static#decode()", () => {
+    context("data type === 8 bits", () => {
+      // TODO:
+    });
+
+    context("data type === 16 bits", () => {
+      // TODO:
+    });
+
+    context("data type === 36 bits", () => {
+      // TODO:
+    });
+  });
+
+  describe("static#fromXmlNode()", () => {
+    // TODO:
+  });
+
+  describe("static#getDefault()", () => {
+    // TODO:
+  });
+});
+
+describe("BigIntCell", function() {
   describe("#clone()", () => {
     // TODO:
   });
