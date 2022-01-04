31a3230e79a63c4ad7a653ba2cd20890a14b300b
Frank Kulak <fs.kulak@gmail.com>
2022-01-03 23:43:55 -0500

    Add mock owner.

---
+++ test/mocks/mockOwner.test.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import MockOwner from "./mockOwner";
+
+describe("MockOwner", function() {
+  it("should be cached when initialized", () => {
+    const owner = new MockOwner();
+    expect(owner.cached).to.be.true;
+  });
+
+  it("should be uncached when uncache() is called", () => {
+    const owner = new MockOwner();
+    owner.uncache();
+    expect(owner.cached).to.be.false;
+  });
+});
---
+++ test/mocks/mockOwner.ts
@@ -0,0 +1,16 @@
+import CacheableModel from "../../dst/lib/models/abstract/cacheableModel";
+
+export default class MockOwner extends CacheableModel {
+  private _cached: boolean;
+
+  get cached() { return this._cached; }
+
+  constructor() {
+    super();
+    this._cached = true;
+  }
+
+  uncache(): void {
+    this._cached = false;
+  }
+}
