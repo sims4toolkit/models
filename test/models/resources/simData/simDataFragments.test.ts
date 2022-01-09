cd4149c26ab8af3213e5b5494b26374e79cf1a4a
Frank Kulak <fs.kulak@gmail.com>
2022-01-09 17:56:32 -0500

    Update simDataFragments.test.ts

---
+++ test/models/resources/simData/simDataFragments.test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import { simDataFragments, simDataTypes } from "../../../../dst/api";
+
+const { SimDataSchema, SimDataSchemaColumn, SimDataInstance } = simDataFragments;
+const { SimDataType } = simDataTypes;
+
+describe("SimDataSchema", () => {
+  // TODO:
+});
+
+describe("SimDataSchemaColumn", () => {
+  // TODO:
+});
+
+describe("SimDataInstance", () => {
+  // TODO:
+});
