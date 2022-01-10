dfef9019ef68351b2964a4a65f501c0ff271b6e6
Frank Kulak <fs.kulak@gmail.com>
2022-01-09 22:41:37 -0500

    Refactor SimData fragments.

--- src/lib/models/resources/simData/simDataFragments.ts
+++ src/lib/models/resources/simData/simDataFragments.ts
@@ -11,96 +11,58 @@ import { formatAsHexString } from "../../../utils/formatting";
  * A schema that objects in a SimData can follow.
  */
 export class SimDataSchema extends CacheableModel {
-  owner?: SimDataResource;
+  public owner?: SimDataResource;
   private _columns: SimDataSchemaColumn[];
 
   /**
-   * The columns in this schema. Individual columns can be mutated and cacheing
-   * will be handled (e.g. `columns[0].name = "col_name"` is perfectly safe),
-   * however, mutating the array itself by adding or removing columns should be
-   * avoided whenever possible, because doing so is a surefire way to mess up
-   * the cache. 
-   * 
-   * To add or remove columns, use the `addColumns()` and `removeColumns()`
-   * methods, or you may also call `delete()` on the column you wish to remove.
-   * 
-   * If you insist on removing from or sorting the array manually, you can, as
-   * long as you remember to call `uncache()` when you are done. If you insist
-   * on adding columns manually, it's your funeral.
+   * An array that contains the columns for this schema. Mutating this array and
+   * its children is safe in terms of cacheing.
    */
   get columns() { return this._columns; }
+  private set columns(columns: SimDataSchemaColumn[]) {
+    const owner = this._getCollectionOwner();
+    columns.forEach(column => column.owner = owner);
+    this._columns = this._getCollectionProxy(columns);
+  }
 
   constructor(public name: string, public hash: number, columns: SimDataSchemaColumn[], owner?: SimDataResource) {
     super(owner);
-    this._columns = columns || [];
-    this.columns.forEach(column => column.owner = this);
+    this.columns = columns ?? [];
     this._watchProps('name', 'hash');
   }
 
-  /**
-   * Creates a new SimDataSchema from the given arguments. This is a wrapper
-   * for the constructor, provided for consistency with the rest of the API.
-   * 
-   * @param arguments Arguments for the schema to create
-   */
-  static create({ name, hash, columns = [] }: {
-    name: string;
-    hash: number;
-    columns?: SimDataSchemaColumn[];
-  }): SimDataSchema {
-    return new SimDataSchema(name, hash, columns);
+  protected _getCollectionOwner(): SimDataResource {
+    return this.owner;
   }
 
   /**
-   * Adds columns to this schema and uncaches it. The provided columns should
-   * be new objects (i.e. columns that are not already part of another schema).
-   * If you want to copy columns from another schema, use `addColumnClones()`
-   * instead, so as to not affect the internal cache.
-   * 
-   * @param columns Columns to add to this schema
-   */
-  addColumns(...columns: SimDataSchemaColumn[]) {
-    columns.forEach(({ name, type, flags = 0 }) => {
-      this.columns.push(new SimDataSchemaColumn(name, type, flags, this));
-    });
-
-    this.uncache();
-  }
-
-  /**
-   * Clones and adds the given columns to this schema. This method ensures that
-   * the columns that get added are new objects, so as to avoid cacheing issues.
+   * Clones and adds the given columns to this schema. It is always recommended
+   * to use this method over a regular `push()` when sharing columns between
+   * schemas, so as to avoid cacheing issues.
    * 
    * @param columns Columns to add to this schema
    */
   addColumnClones(...columns: SimDataSchemaColumn[]) {
-    this.addColumns(...(columns.map(column => column.clone())));
+    this.columns.push(...(columns.map(column => column.clone())));
   }
 
   /**
-   * Removes the given columns from this schema and then uncaches it. Columns
-   * are removed by reference equality, so the given objects must be the exact
-   * objects that exist in this SimData.
+   * Creates a deep copy of this schema, with all of its contents (except for
+   * its owner) being copied.
+   */
+  clone(): SimDataSchema {
+    return new SimDataSchema(this.name, this.hash, this.columns.map(column => column.clone()));
+  }
+
+  /**
+   * Removes the given columns from this schema. Columns are removed by
+   * reference equality, so the given objects must be the exact objects that
+   * exist in this SimData.
    * 
    * @param columns Columns to remove to this schema
    */
   removeColumns(...columns: SimDataSchemaColumn[]) {
-    if(removeFromArray(columns, this.columns)) this.uncache();
-  }
-
-  /**
-   * Removes this schema from its owning SimDataResource, if it has one.
-   */
-  delete() {
-    this.owner?.removeSchemas(this); // removeSchemas() uncaches
-  }
-
-  /**
-   * Creates a deep copy of this schema, with all values except for owner being
-   * copied over.
-   */
-  clone(): SimDataSchema {
-    return new SimDataSchema(this.name, this.hash, this.columns.map(column => column.clone()));
+    removeFromArray(columns, this.columns);
   }
 
   /**
@@ -151,33 +113,11 @@ export class SimDataSchema extends CacheableModel {
  * A column in a SimData schema.
  */
 export class SimDataSchemaColumn extends CacheableModel {
-  owner?: SimDataSchema;
+  public owner?: SimDataResource;
 
-  constructor(public name: string, public type: SimDataType, public flags: number, owner?: SimDataSchema) {
+  constructor(public name: string, public type: SimDataType, public flags: number, owner?: SimDataResource) {
     super(owner);
     this._watchProps('name', 'type', 'flags');
-  }
-
-  /**
-   * Creates a new SimDataSchemaColumn from the given arguments. This is a
-   * wrapper for the constructor, provided for consistency with the rest of the
-   * API.
-   * 
-   * @param arguments Arguments for the column to create
-   */
-  static create({ name, type, flags = 0 }: {
-    name: string;
-    type: SimDataType;
-    flags?: number;
-  }): SimDataSchemaColumn {
-    return new SimDataSchemaColumn(name, type, flags);
-  }
-
-  /**
-   * Removes this column from its owning SimDataSchema, if it has one.
-   */
-  delete(): void {
-    this.owner?.removeColumns(this); // removeColumns() uncaches
   }
 
   /**
@@ -228,48 +168,16 @@ export class SimDataSchemaColumn extends CacheableModel {
  * a SimData XML, so these are the models that users are familiar with.
  */
 export class SimDataInstance extends ObjectCell {
-  owner?: SimDataResource;
+  public owner?: SimDataResource;
 
   constructor(public name: string, schema: SimDataSchema, row: ObjectCellRow, owner?: SimDataResource) {
     super(schema, row, owner);
     this._watchProps('name');
   }
 
-  /**
-   * Creates a new SimDataInstance from the given arguments. This is a wrapper
-   * for the constructor, provided for consistency with the rest of the API.
-   * 
-   * @param arguments Arguments for the instance to create
-   */
-  static create({ name, schema, row }: {
-    name: string;
-    schema: SimDataSchema;
-    row: ObjectCellRow;
-  }): SimDataInstance {
-    return new SimDataInstance(name, schema, row);
-  }
-
-  /**
-   * Removes this instance from its owning SimDataResource, if it has one.
-   */
-  delete(): void {
-    this.owner?.removeInstances(this); // removeInstances() uncaches
-  }
-
   clone(options?: CellCloneOptions): SimDataInstance {
     const { schema, row } = this._internalClone(options);
     return new SimDataInstance(this.name, schema, row);
-  }
-
-  /**
-   * Creates a SimDataInstance from an ObjectCell. Nothing is cloned - exact
-   * references are maintained. Additionally, the owner is not copied.
-   * 
-   * @param source ObjectCell to base this instance off of
-   */
-  static fromObjectCell(name: string, source: ObjectCell): SimDataInstance {
-    // FIXME: proxy of a proxy.. is this a concern anywhere else?
-    return new SimDataInstance(name, source.schema, source.row);
   }
 
   /**
@@ -288,6 +196,16 @@ export class SimDataInstance extends ObjectCell {
         return this.row[column.name].toXmlNode({ nameAttr: column.name });
       })
     });
+  }
+
+  /**
+   * Creates a SimDataInstance from an ObjectCell. Nothing is cloned - exact
+   * references are maintained. Additionally, the owner is not copied.
+   * 
+   * @param source ObjectCell to base this instance off of
+   */
+  static fromObjectCell(name: string, source: ObjectCell): SimDataInstance {
+    return new SimDataInstance(name, source.schema, source.row);
   }
 
   /**
@@ -320,7 +238,7 @@ function parseNodeAttrAsNumber(node: XmlNode, attr: string): number {
   const raw = node.attributes[attr];
   const value = parseInt(raw, 16);
   if (Number.isNaN(value))
-    throw new Error(`Expected value of '${attr}' to be a number, got ${raw}`);
+    throw new Error(`Expected value of '${attr}' to be a hex number, got ${raw}`);
   return value;
 }
 
