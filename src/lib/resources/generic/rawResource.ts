7ab34b0d1ddac3342429dbdf71d2bf90b73a113d
Frank Kulak <fs.kulak@gmail.com>
2022-01-11 21:14:40 -0500

    Update rawResource.ts

--- src/lib/resources/generic/rawResource.ts
+++ src/lib/resources/generic/rawResource.ts
@@ -8,11 +8,23 @@ export default class RawResource extends Resource {
   private _reason?: string;
   private _content?: string;
 
+  /** The contents of this resource as plain text. */
+   get plainText(): string {
+    return this._content ??= this.buffer.toString('utf-8');
+  }
+
+  /** Reason why this resource has not been parsed. */
   get reason(): string {
     return this._reason;
   }
 
-  protected constructor(buffer: Buffer, reason?: string) {
+  /**
+   * Creates a new RawResource instance.
+   * 
+   * @param buffer Buffer to load into this resource
+   * @param reason Reason why this resource is being loaded raw
+   */
+  constructor(buffer: Buffer, reason?: string) {
     super({ buffer });
     this._reason = reason;
   }
@@ -21,8 +33,16 @@ export default class RawResource extends Resource {
     return RawResource.from(this.buffer);
   }
 
+  uncache() {
+    // intentionally blank because this resource cannot be uncached
+  }
+
+  protected _serialize(): Buffer {
+    throw new Error("Cannot serialize a raw resource.");
+  }
+
   /**
-   * Creates a new raw resource from the given buffer. This is functionally the
+   * Creates a new RawResource from the given buffer. This is functionally the
    * same as the constructor, but is provided for parity with the other resource
    * types.
    * 
@@ -30,23 +50,5 @@ export default class RawResource extends Resource {
    */
   static from(buffer: Buffer, reason?: string): RawResource {
     return new RawResource(buffer, reason);
-  }
-
-  protected _serialize(): Buffer {
-    throw new Error("Cannot serialize a raw resource.");
-  }
-
-  uncache() {
-    // intentionally blank because this resource cannot be uncached
-  }
-
-  /**
-   * Returns this resource as plain text. This is a lazy function, in that the
-   * content will not be decoded until this function is called.
-   */
-  get plainText(): string {
-    if (this._content === undefined)
-      this._content = this.buffer.toString('utf-8');
-    return this._content;
   }
 }
