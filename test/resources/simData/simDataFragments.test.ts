1ea6c1dee95fae12871e5012098bd6688b260df1
Frank Kulak <fs.kulak@gmail.com>
2022-01-12 21:34:07 -0500

    Add property and method stubs to simdata fragment tests.

--- test/resources/simData/simDataFragments.test.ts
+++ test/resources/simData/simDataFragments.test.ts
@@ -5,13 +5,99 @@ const { SimDataSchema, SimDataSchemaColumn, SimDataInstance } = simDataFragments
 const { SimDataType } = simDataTypes;
 
 describe("SimDataSchema", () => {
-  // TODO:
+  describe("#owner", () => {
+    // TODO:
+  });
+
+  describe("#columns", () => {
+    // TODO:
+  });
+
+  describe("#hash", () => {
+    // TODO:
+  });
+
+  describe("#name", () => {
+    // TODO:
+  });
+
+  describe("#constructor", () => {
+    // TODO:
+  });
+
+  describe("#addColumnClones()", () => {
+    // TODO:
+  });
+
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#removeColumns()", () => {
+    // TODO:
+  });
+
+  describe("#toXmlNode()", () => {
+    // TODO:
+  });
+
+  describe("static#fromXmlNode()", () => {
+    // TODO:
+  });
 });
 
 describe("SimDataSchemaColumn", () => {
-  // TODO:
+  describe("#owner", () => {
+    // TODO:
+  });
+
+  describe("#flags", () => {
+    // TODO:
+  });
+
+  describe("#name", () => {
+    // TODO:
+  });
+
+  describe("#type", () => {
+    // TODO:
+  });
+
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#removeColumns()", () => {
+    // TODO:
+  });
+
+  describe("static#fromXmlNode()", () => {
+    // TODO:
+  });
 });
 
 describe("SimDataInstance", () => {
-  // TODO:
+  describe("#owner", () => {
+    // TODO:
+  });
+
+  describe("#name", () => {
+    // TODO:
+  });
+
+  describe("#clone", () => {
+    // TODO:
+  });
+
+  describe("#toXmlNode()", () => {
+    // TODO:
+  });
+
+  describe("static#fromObjectCell()", () => {
+    // TODO:
+  });
+
+  describe("static#fromXmlNode()", () => {
+    // TODO:
+  });
 });
