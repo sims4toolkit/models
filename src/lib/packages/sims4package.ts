f6fbc5e65a010e4545509f9e919579810ff64a6b
Frank Kulak <fs.kulak@gmail.com>
2022-01-18 20:21:48 -0500

    Update sims4package.ts

--- src/lib/packages/sims4package.ts
+++ src/lib/packages/sims4package.ts
@@ -12,64 +12,6 @@ import readDbpf from "./serialization/readDbpf";
 import writeDbpf from "./serialization/writeDbpf";
 
 /**
- * An entry for a resource in a package file. This entry has a key and handles
- * compression of the buffer.
- */
-class ResourceEntry extends WritableModel implements MappedModelEntry<ResourceKey, Resource> {
-  public owner: Sims4Package;
-  private _key: ResourceKey;
-  private _resource: Resource;
-
-  get key(): ResourceKey { return this._key; }
-  set key(key: ResourceKey) {
-    const owner = this._getCollectionOwner();
-    const onChange = (target: ResourceKey, property: string, previous: any) => {
-      const old = clone(target);
-      old[property] = previous;
-      owner.onKeyUpdate(old, target);
-    };
-
-    if (this._key) owner.onKeyUpdate(this._key, key);
-    this._key = this._getCollectionProxy(key, onChange);
-  }
-
-  /** The resource in this entry. */
-  get value(): Resource { return this._resource; }
-  set value(resource: Resource) {
-    if (resource) resource.owner = this;
-    this._resource = resource;
-    this.uncache();
-  }
-  
-  constructor(key: ResourceKey, resource: Resource, owner?: Sims4Package) {
-    super({ owner });
-    this.key = key;
-    this.value = resource;
-    // key and value are watched manually in their setters
-  }
-
-  clone(): ResourceEntry {
-    return new ResourceEntry(clone(this.key), this.value.clone());
-  }
-
-  equals(other: ResourceEntry): boolean {
-    return this.keyEquals(other?.key) && this.value.equals(other?.value);
-  }
-
-  keyEquals(key: ResourceKey): boolean {
-    return compare(this.key, key);
-  }
-
-  protected _getCollectionOwner(): Sims4Package {
-    return this.owner;
-  }
-
-  protected _serialize(): Buffer {
-    return deflateSync(this.value.buffer);
-  }
-}
-
-/**
  * Model for a Sims 4 package file (also called a "Database Packed File", or
  * DBPF for short).
  */
@@ -153,4 +95,62 @@ export default class Sims4Package extends MappedModel<ResourceKey, Resource, Res
   }
 
   //#endregion Protected Methods
+}
+
+/**
+ * An entry for a resource in a package file. This entry has a key and handles
+ * compression of the buffer.
+ */
+class ResourceEntry extends WritableModel implements MappedModelEntry<ResourceKey, Resource> {
+  public owner: Sims4Package;
+  private _key: ResourceKey;
+  private _resource: Resource;
+
+  get key(): ResourceKey { return this._key; }
+  set key(key: ResourceKey) {
+    const owner = this._getCollectionOwner();
+    const onChange = (target: ResourceKey, property: string, previous: any) => {
+      const old = clone(target);
+      old[property] = previous;
+      owner.onKeyUpdate(old, target);
+    };
+
+    if (this._key) owner.onKeyUpdate(this._key, key);
+    this._key = this._getCollectionProxy(key, onChange);
+  }
+
+  /** The resource in this entry. */
+  get value(): Resource { return this._resource; }
+  set value(resource: Resource) {
+    if (resource) resource.owner = this;
+    this._resource = resource;
+    this.uncache();
+  }
+  
+  constructor(key: ResourceKey, resource: Resource, owner?: Sims4Package) {
+    super({ owner });
+    this.key = key;
+    this.value = resource;
+    // key and value are watched manually in their setters
+  }
+
+  clone(): ResourceEntry {
+    return new ResourceEntry(clone(this.key), this.value.clone());
+  }
+
+  equals(other: ResourceEntry): boolean {
+    return this.keyEquals(other?.key) && this.value.equals(other?.value);
+  }
+
+  keyEquals(key: ResourceKey): boolean {
+    return compare(this.key, key);
+  }
+
+  protected _getCollectionOwner(): Sims4Package {
+    return this.owner;
+  }
+
+  protected _serialize(): Buffer {
+    return deflateSync(this.value.buffer);
+  }
 }
