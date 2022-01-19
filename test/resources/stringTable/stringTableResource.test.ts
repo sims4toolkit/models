ee2b78df7621cc141333649fa816a65d2012da1f
Frank Kulak <fs.kulak@gmail.com>
2022-01-19 11:57:24 -0500

    Add stubs for stbl tests.

--- test/resources/stringTable/stringTableResource.test.ts
+++ test/resources/stringTable/stringTableResource.test.ts
@@ -20,5 +20,145 @@ function getStbl(filename: string): StringTableResource {
 //#endregion Helpers
 
 describe("StringTableResource", () => {
-  // TODO:
+  //#region Properties
+
+  describe("#buffer", () => {
+    // TODO:
+  });
+
+  describe("#entries", () => {
+    // TODO:
+  });
+
+  describe("#hasChanged", () => {
+    // TODO:
+  });
+
+  describe("#header", () => {
+    // TODO:
+  });
+  
+  describe("#owner", () => {
+    // TODO:
+  });
+
+  describe("#size", () => {
+    // TODO:
+  });
+
+  //#endregion Properties
+
+  //#region Initialization
+
+  describe("static#create()", () => {
+    // TODO:
+  });
+
+  describe("static#from()", () => {
+    // TODO:
+  });
+
+  //#endregion Initialization
+
+  //#region Methods
+
+  describe("#add()", () => {
+    // TODO:
+  });
+
+  describe("#addAll()", () => {
+    // TODO:
+  });
+
+  describe("#addAndHash()", () => {
+    // TODO:
+  });
+
+  describe("#clear()", () => {
+    // TODO:
+  });
+
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#delete()", () => {
+    // TODO:
+  });
+
+  describe("#deleteByKey()", () => {
+    // TODO:
+  });
+
+  describe("#equals()", () => {
+    // TODO:
+  });
+
+  describe("#get()", () => {
+    // TODO:
+  });
+
+  describe("#getByKey()", () => {
+    // TODO:
+  });
+
+  describe("#getIdForKey()", () => {
+    // TODO:
+  });
+
+  describe("#getIdsForKey()", () => {
+    // TODO:
+  });
+
+  describe("#hasKey()", () => {
+    // TODO:
+  });
+
+  describe("#resetEntries()", () => {
+    // TODO:
+  });
+
+  describe("#uncache()", () => {
+    // TODO:
+  });
+
+  describe("#validate()", () => {
+    // TODO:
+  });
+
+  //#endregion Methods
+});
+
+describe("StringEntry", () => {
+  describe("#key", () => {
+    // TODO:
+  });
+
+  describe("#owner", () => {
+    // TODO:
+  });
+
+  describe("#value", () => {
+    // TODO:
+  });
+
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#equals()", () => {
+    // TODO:
+  });
+
+  describe("#keyEquals()", () => {
+    // TODO:
+  });
+
+  describe("#uncache()", () => {
+    // TODO:
+  });
+
+  describe("#validate()", () => {
+    // TODO:
+  });
 });
