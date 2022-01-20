d2260f3f66beedded544faaeb682ea6d6d3d11a8
Frank Kulak <fs.kulak@gmail.com>
2022-01-20 09:05:53 -0500

    Create sims4package.test.ts

---
+++ test/dbpf/sims4package.test.ts
@@ -0,0 +1,10 @@
+import { Sims4Package } from "../../dst/api";
+import { expect } from "chai";
+
+describe("Sims4Package", () => {
+  // TODO:
+});
+
+describe("ResourceEntry", () => {
+  // TODO:
+});
