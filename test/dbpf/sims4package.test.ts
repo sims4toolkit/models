4552ad8472e2c65da9a8210f2a6d5d07151627ad
Frank Kulak <fs.kulak@gmail.com>
2022-01-20 12:10:29 -0500

    Update sims4package.test.ts

--- test/dbpf/sims4package.test.ts
+++ test/dbpf/sims4package.test.ts
@@ -2,5 +2,111 @@ import { Sims4Package } from "../../dst/api";
 import { expect } from "chai";
 
 describe("Sims4Package", () => {
-  // TODO:
+  //#region Properties
+
+  describe("#buffer", () => {
+    // TODO:
+  });
+
+  describe("#entries", () => {
+    // TODO:
+  });
+
+  // #hasChanged tested by other tests
+
+  describe("#header", () => {
+    // TODO:
+  });
+
+  // #isCached tested by other tests
+
+  describe("#size", () => {
+    // TODO:
+  });
+
+  //#endregion Properties
+
+  //#region Initialization
+
+  describe("static#create()", () => {
+    // TODO:
+  });
+
+  describe("static#from()", () => {
+    // TODO:
+  });
+
+  //#endregion Initialization
+
+  //#region Public Methods
+
+  describe("#add()", () => {
+    // TODO:
+  });
+
+  describe("#addAll()", () => {
+    // TODO:
+  });
+
+  describe("#clear()", () => {
+    // TODO:
+  });
+
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#equals()", () => {
+    // TODO:
+  });
+
+  describe("#delete()", () => {
+    // TODO:
+  });
+
+  describe("#deleteByKey()", () => {
+    // TODO:
+  });
+
+  describe("#findRepeatedKeys()", () => {
+    // TODO:
+  });
+
+  describe("#get()", () => {
+    // TODO:
+  });
+
+  describe("#getByKey()", () => {
+    // TODO:
+  });
+
+  describe("#getIdForKey()", () => {
+    // TODO:
+  });
+
+  describe("#getIdsForKey()", () => {
+    // TODO:
+  });
+
+  describe("#has()", () => {
+    // TODO:
+  });
+
+  describe("#hasKey()", () => {
+    // TODO:
+  });
+
+  describe("#resetEntries()", () => {
+    // TODO:
+  });
+
+  describe("#uncache()", () => {
+    // TODO:
+  });
+
+  describe("#validate()", () => {
+    // TODO:
+  });
+
+  //#endregion Public Methods
 });
