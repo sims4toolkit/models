3c9a79ade25e257c0493e3dfcad193826e136c69
Frank Kulak <fs.kulak@gmail.com>
2022-01-20 18:31:17 -0500

    Remove resource entry from api.

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,4 @@
 import Sims4Package from "./lib/dbpf/sims4package";
-import ResourceEntry from "./lib/dbpf/resourceEntry";
 import XmlResource from "./lib/resources/generic/xmlResource";
 import RawResource from "./lib/resources/generic/rawResource";
 import TuningResource from "./lib/resources/tuning/tuningResource";
@@ -14,7 +13,6 @@ import * as simDataTypes from "./lib/resources/simData/simDataTypes";
 export {
   // Models
   Sims4Package,
-  ResourceEntry,
   RawResource,
   XmlResource,
   TuningResource,
