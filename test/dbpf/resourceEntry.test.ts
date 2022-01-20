8574995a24ac7d4eb2b10d35d4d1503c0d196586
Frank Kulak <fs.kulak@gmail.com>
2022-01-20 12:04:29 -0500

    Update resourceEntry.test.ts

--- test/dbpf/resourceEntry.test.ts
+++ test/dbpf/resourceEntry.test.ts
@@ -2,5 +2,55 @@ import { ResourceEntry } from "../../dst/api";
 import { expect } from "chai";
 
 describe("ResourceEntry", () => {
-  // TODO:
+  //#region Properties
+
+  describe("#buffer", () => {
+    // TODO:
+  });
+
+  // #hasChanged tested by other tests
+
+  describe("#key", () => {
+    // TODO:
+  });
+
+  // #isCached tested by other tests
+
+  describe("#value", () => {
+    // TODO:
+  });
+
+  //#endregion Properties
+
+  //#region Initialization
+
+  describe("#constructor", () => {
+    // TODO:
+  });
+
+  //#endregion Initialization
+
+  //#region Public Methods
+
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#equals()", () => {
+    // TODO:
+  });
+
+  describe("#keyEquals()", () => {
+    // TODO:
+  });
+
+  describe("#uncache()", () => {
+    // TODO:
+  });
+
+  describe("#validate()", () => {
+    // TODO:
+  });
+
+  //#endregion Public Methods
 });
