4ffeb910f6f560859afe7edff2e8e62c9b759ab0
Frank Kulak <fs.kulak@gmail.com>
2022-01-21 23:07:58 -0500

    Add SimDataGroup enum.

--- src/lib/enums/resourceTypes.ts
+++ src/lib/enums/resourceTypes.ts
@@ -13,3 +13,11 @@ export enum TuningResourceType {
   Buff = 0x6017E896,
   Trait = 0xCB5FDDC7,
 }
+
+/**
+ * Groups for SimDatas.
+ */
+export enum SimDataGroup {
+  Buff = 0x0017E8F6,
+  Trait = 0x005FDD0C
+}
