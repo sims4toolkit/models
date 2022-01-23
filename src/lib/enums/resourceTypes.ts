7c40b7c8e1425480f572356b92579a9ad8ec5d77
Frank Kulak <fs.kulak@gmail.com>
2022-01-23 17:39:42 -0500

    Add binary tuning resource type.

--- src/lib/enums/resourceTypes.ts
+++ src/lib/enums/resourceTypes.ts
@@ -2,6 +2,7 @@
  * Types for binary files.
  */
 export enum BinaryResourceType {
+  CombinedBinaryTuning = 0x62E94D38,
   DstImage = 0x00B2D882,
   SimData = 0x545AC67A,
   StringTable = 0x220557DA,
