fa7a5fe8f691f9422c5d0e6151d15823ba825050
Frank Kulak <fs.kulak@gmail.com>
2022-01-24 17:31:04 -0500

    Update options documentation.

--- src/lib/common/options.ts
+++ src/lib/common/options.ts
@@ -3,22 +3,29 @@
  */
 export interface FileReadingOptions {
   /**
-   * If `true`, then non-critical errors will be ignored, and parsing will
-   * continue until termination or a fatal error. This is `false` by default.
+   * If true, non-critical errors will be ignored while reading the file. False
+   * by default.
+   * 
+   * Note that this option does NOT prevent exceptions from being thrown. Its
+   * purpose is to ignore negligible errors, such as an incorrect file version
+   * or an issue with the file header. Critical errors, such as missing data in
+   * a binary file, will still cause an exception.
    */
   ignoreErrors?: boolean;
 
   /**
-   * If `true`, then the result of a resource's static `from()` method will not
-   * throw an error. If there is an error, the function will return `undefined`
-   * instead. This is `false` by default.
+   * If true, models that cannot be read will be `undefined`, rather than
+   * throwing an exception. False by default.
    */
   dontThrow?: boolean;
 
   /**
-   * (For packages only) If `true`, then the resources within this package will
-   * all be loaded as raw resources, and can only be read as plain text. This is
-   * `false` by default.
+   * (For packages only) If true, then all resources in the package will be
+   * loaded as a raw resource (i.e. just a buffer) rather than being fully
+   * parsed into its respective model. False by default.
+   * 
+   * This is recommended when extracting files from packages, so as to reduce
+   * the computation load.
    */
   loadRaw?: boolean;
 }
