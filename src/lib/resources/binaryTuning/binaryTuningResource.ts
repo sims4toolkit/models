8c18603a1060b70a437ab50ee524fe99f8fe659f
Frank Kulak <fs.kulak@gmail.com>
2022-01-23 23:25:06 -0500

    Implement binary tuning reading.

---
+++ src/lib/resources/binaryTuning/binaryTuningResource.ts
@@ -0,0 +1,23 @@
+import Resource from "../resource";
+import { BinaryTuningDto } from "./shared";
+
+/**
+ * TODO:
+ */
+export default class BinaryTuningResource extends Resource {
+  readonly variant = "DATA";
+  public data: BinaryTuningDto;
+
+  static from(buffer: Buffer): BinaryTuningResource {
+    // TODO:
+    return;
+  }
+  
+  clone(): Resource {
+    throw new Error("Method not implemented.");
+  }
+
+  protected _serialize(): Buffer {
+    throw new Error("Method not implemented.");
+  }
+}
