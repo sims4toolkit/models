74cb6adcd816279d0faf09f19b7df6beb4c10356
Frank Kulak <fs.kulak@gmail.com>
2022-01-25 14:47:47 -0500

    Update file reading options.

--- src/lib/common/options.ts
+++ src/lib/common/options.ts
@@ -1,52 +1,53 @@
 /**
  * Options to configure when reading a file.
  */
-export interface FileReadingOptions {
+export interface FileReadingOptions {  
   /**
-   * (For packages only) If true, then the compressed buffer for a resource will
-   * not be cached in the resource entry. False by default.
-   * 
-   * This is recommended when loading packages in read-only mode, or while
-   * extracting resources from packages, so as to reduce the memory load.
-   * Cacheing the compressed buffer is only worthwhile when a package is
-   * actively being edited, or when a small subset of resources are being
-   * edited within it.
-   */
-  dontCacheCompressedBuffer?: boolean;
-
-  /**
-   * If true, then the decompressed buffer for a resource will not be cached.
-   * False by default.
-   * 
-   * This is recommended when loading packages or resources in read-only mode,
-   * so as to reduce the memory load.
-   */
-  dontCacheResource?: boolean;
-
-  /**
-   * If true, models that cannot be read will be `undefined`, rather than
-   * throwing an exception. False by default.
-   */
-  dontThrow?: boolean;
-  
-  /**
-   * If true, non-critical errors will be ignored while reading the file. False
-   * by default.
+   * (For binary files only) If true, non-critical errors will be ignored while
+   * reading the file(s). False by default.
    * 
    * Note that this option does NOT prevent exceptions from being thrown. Its
    * purpose is to ignore negligible errors, such as an incorrect file version
-   * or an issue with the file header. Critical errors, such as missing data in
-   * a binary file, will still cause an exception.
+   * or an issue with the file header. Critical errors, such as missing data or
+   * incorrect data type, will still cause an exception.
    */
   ignoreErrors?: boolean;
+
+  /**
+   * (For packages only) If true, then any erred resources will be loaded raw
+   * instead of causing an exception. False by default.
+   */
+  loadErrorsAsRaw?: boolean;
 
   /**
    * (For packages only) If true, then all resources in the package will be
    * loaded as a raw resource (i.e. just a buffer) rather than being fully
    * parsed into its respective model. False by default.
    * 
-   * This is recommended when extracting files from packages, so as to reduce
-   * the computation load.
+   * This is recommended when extracting files from packages, so that fully
+   * parsed models are not needlessly created. If there is a need for fully
+   * parsed models, consider using the `saveBuffer` option instead.
    */
   loadRaw?: boolean;
+
+  /**
+   * If true, then the decompressed buffer for a resource will be saved on its
+   * model. Note that raw resources always save their buffer regardless of this
+   * option, as it is their only defining feature. False by default.
+   * 
+   * This is recommended when parsed models are required while extracting files
+   * from packages. If there is no need to parse the models and just the buffers
+   * are needed, consider using the `loadRaw` option instead so that time is not
+   * wasted parsing models.
+   */
+  saveBuffer?: boolean;
+
+  /**
+   * (For packages only) If true, then the compressed buffer for a resource will
+   * be saved on its resource entry. False by default.
+   * 
+   * This is recommended when editing a subset of the resources in a package and
+   * then writing it back to disk.
+   */
+  saveCompressedBuffer?: boolean;
 }
