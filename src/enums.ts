225a307c187118e5a47719b5bdd78a45c3ab3589
Frank Kulak <fs.kulak@gmail.com>
2022-01-27 12:39:39 -0500

    Get rid of SimDataGroup enum.

--- src/enums.ts
+++ src/enums.ts
@@ -1,13 +1,11 @@
 import BinaryResourceType from "./lib/enums/binary-resources";
 import EncodingType from "./lib/enums/encoding-type";
-import SimDataGroup from "./lib/enums/simdata-groups";
 import StringTableLocale from "./lib/enums/stbl-locales";
 import TuningResourceType from "./lib/enums/tuning-resources";
 
 export {
   BinaryResourceType,
   EncodingType,
-  SimDataGroup,
   StringTableLocale,
   TuningResourceType,
 }
