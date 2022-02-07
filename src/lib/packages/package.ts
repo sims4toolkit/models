8f31f2927c95b84c5457631c31480d021de9c715
Frank Kulak <fs.kulak@gmail.com>
2022-02-07 14:52:28 -0500

    Temporarily remove the extractResources methods from package.

--- src/lib/packages/package.ts
+++ src/lib/packages/package.ts
@@ -39,7 +39,7 @@ export default class Package extends MappedModel<ResourceKey, Resource, Resource
    * 
    * @param options Optional arguments for creating a new Package
    */
-  static create({ entries, cloneKeys = false, cloneResources = false }: {
+  static create({ entries = [], cloneKeys = false, cloneResources = false }: {
     entries?: ResourceKeyPair[];
     cloneKeys?: boolean;
     cloneResources?: boolean;
@@ -58,46 +58,6 @@ export default class Package extends MappedModel<ResourceKey, Resource, Resource
     }
 
     return new Package(entriesToUse);
-  }
-
-  /**
-   * Reads the given buffer as a Package, but just returns its entries rather
-   * than a full Package object.
-   * 
-   * If extracting resources to write them to disk, consider these options:
-   * - Set `loadRaw: true`, so that resources are loaded as buffers only rather
-   * than being parsed into models.
-   * - If you cannot use `loadRaw` because you need to parse the resources into
-   * models, set `saveBuffer: true` so that they can be written without needing
-   * to re-serialize the model.
-   * - Consider using `resourceFilter` to determine which resources should be
-   * extracted, according to their type, group, and/or instance.
-   * 
-   * @param buffer Buffer to extract resources from
-   * @param options Options for reading and cacheing the resources
-   */
-  static extractResources(buffer: Buffer, options?: FileReadingOptions): ResourceKeyPair[] {
-    return readDbpf(buffer, options);
-  }
-
-  /**
-   * Reads the given buffer as a Package asynchronously, and returns a Promise
-   * that resolves with its entries in an array.
-   * 
-   * If extracting resources to write them to disk, consider these options:
-   * - Set `loadRaw: true`, so that resources are loaded as buffers only rather
-   * than being parsed into models.
-   * - If you cannot use `loadRaw` because you need to parse the resources into
-   * models, set `saveBuffer: true` so that they can be written without needing
-   * to re-serialize the model.
-   * - Consider using `resourceFilter` to determine which resources should be
-   * extracted, according to their type, group, and/or instance.
-   * 
-   * @param buffer Buffer to extract resources from
-   * @param options Options for reading and cacheing the resources
-   */
-  static async extractResourcesAsync(buffer: Buffer, options?: FileReadingOptions): Promise<ResourceKeyPair[]> {
-    return promisify(() => Package.extractResources(buffer, options));
   }
 
   /**
