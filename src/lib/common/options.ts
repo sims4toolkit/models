e5bfc094bed7b6f46799506337ab6e0d05cb81a2
Frank Kulak <fs.kulak@gmail.com>
2022-04-23 16:38:27 -0400

    Update options.ts

--- src/lib/common/options.ts
+++ src/lib/common/options.ts
@@ -6,7 +6,7 @@ export type ResourceFilter = (type: number, group: number, instance: bigint) => 
 
 
 /**
- * Options to configure when reading a file.
+ * Options to configure when reading files.
  */
 export interface FileReadingOptions {
   /**
@@ -19,23 +19,6 @@ export interface FileReadingOptions {
   decompressBuffer?: boolean;
 
   /**
-   * (For binary files only) If true, non-critical errors will be ignored while
-   * reading the file(s), and an exception will only be thrown when it is
-   * totally unavoidable. False by default.
-   * 
-   * Recommended when recovering potentially corrupt resources from a package.
-   */
-  ignoreErrors?: boolean;
-
-  /**
-   * (For packages only) If true, then any erred resources will be loaded raw
-   * (i.e. just a buffer) instead of causing an exception. False by default.
-   * 
-   * Recommended when recovering potentially corrupt resources from a package.
-   */
-  loadErrorsAsRaw?: boolean;
-
-  /**
    * (For packages only) If true, then all resources in the package will be
    * loaded raw (i.e. just a buffer) rather than being fully parsed into their
    * respective models. False by default.
@@ -45,6 +28,16 @@ export interface FileReadingOptions {
    * files to write to disk), set `decompressBuffer: true` as well.
    */
   loadRaw?: boolean;
+
+  /**
+   * (For binary files only) If true, non-critical errors will be ignored while
+   * reading the file(s), and exceptions will only be thrown when totally
+   * unavoidable. If being used on a package, any erred resources will be loaded
+   * raw (i.e. just a buffer) instead of causing an exception. False by default.
+   * 
+   * Recommended when recovering potentially corrupt resources from a package.
+   */
+  recoveryMode?: boolean;
 
   /**
    * (For packages only) If provided, then the only resources that will be
@@ -60,11 +53,7 @@ export interface FileReadingOptions {
    * If true, then the buffer for this resource (or all resources, if used with
    * a package) will be saved on its model. The buffer is cached when the
    * resource is initially loaded, uncached whenever a change is detected, and
-   * re-cached the next time it is serialized (i.e. next time the `buffer`
-   * property is accessed). False by default.
-   * 
-   * Whether the buffer being saved is compressed or not depends on what the
-   * `decompressBuffer` option is set to. It is also false by default.
+   * re-cached the next time it is serialized. False by default.
    * 
    * It is recommended to save compressed buffers whenever entire packages are
    * going to be written, and it is recommended to save decompressed buffers
