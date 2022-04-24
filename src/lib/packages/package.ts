ddd67f7bbcd0483908350eb91749ff0a5b17ed84
Frank Kulak <fs.kulak@gmail.com>
2022-04-24 16:46:20 -0400

    Refactor packages.

--- src/lib/packages/package.ts
+++ src/lib/packages/package.ts
@@ -1,12 +1,11 @@
 import type Resource from "../resources/resource";
 import type { ResourceKey, ResourceKeyPair } from "./types";
-import type { FileReadingOptions } from "../common/options";
+import type { PackageFileReadingOptions } from "../common/options";
 import { MappedModel } from "../base/mapped-model";
 import { arraysAreEqual, promisify } from "../common/helpers";
 import readDbpf from "./serialization/read-dbpf";
 import writeDbpf from "./serialization/write-dbpf";
 import ResourceEntry from "./resource-entry";
-import clone from "just-clone";
 
 /**
  * Model for a Sims 4 package file (also called a "Database Packed File", or
@@ -15,44 +14,33 @@ import clone from "just-clone";
 export default class Package extends MappedModel<ResourceKey, Resource, ResourceEntry> {
   //#region Initialization
 
-  protected constructor(entries?: ResourceKeyPair[]) {    
-    super(entries, false, undefined, undefined); // never cache package buffer, it's pointless
+  /**
+   * Creates a new Package from the given entries.
+   * 
+   * @param entries Entries to initialize Package with
+   */
+  constructor(entries?: ResourceKeyPair[]) {
+    super(entries); // never cache package buffer, it's pointless
   }
 
   /**
-   * Creates a new Package instance from the given entries. If none are
-   * provided, the package is empty.
+   * Reads the given buffer as a Package.
    * 
-   * Options
-   * - `entries`: Array of entries to add. New entry objects will be created for
-   * the package, but keys and resources will not be cloned unless told to with
-   * the `cloneKeys` and `cloneResources` options. Empty by default.
-   * - `cloneKeys`: Whether or not to clone the keys before adding them to the
-   * package. False by default.
-   * - `cloneResources`: Whether or not to clone the resource before adding them
-   * to the package. False by default.
-   * 
-   * @param options Optional arguments for creating a new Package
+   * @param buffer Buffer to read as a package
+   * @param options Options for reading and cacheing the resources
    */
-  static create({ entries = [], cloneKeys = false, cloneResources = false }: {
-    entries?: ResourceKeyPair[];
-    cloneKeys?: boolean;
-    cloneResources?: boolean;
-  } = {}): Package {
-    let entriesToUse: ResourceKeyPair[];
+  static from(buffer: Buffer, options?: PackageFileReadingOptions): Package {
+    return new Package(readDbpf(buffer, options));
+  }
 
-    if (cloneKeys || cloneResources) {
-      entriesToUse = entries.map(entry => {
-        return {
-          key: cloneKeys ? clone(entry.key) : entry.key,
-          value: cloneResources ? entry.value.clone() : entry.value
-        }
-      });
-    } else {
-      entriesToUse = entries;
-    }
-
-    return new Package(entriesToUse);
+  /**
+    * Asynchronously reads the given buffer as a Package.
+    * 
+    * @param buffer Buffer to read as a package
+    * @param options Options for reading and cacheing the resources
+    */
+  static async fromAsync(buffer: Buffer, options?: PackageFileReadingOptions): Promise<Package> {
+    return promisify(() => Package.from(buffer, options));
   }
 
   /**
@@ -62,40 +50,19 @@ export default class Package extends MappedModel<ResourceKey, Resource, Resource
    * @param buffer Buffer to extract resources from
    * @param options Options for reading and cacheing the resources
    */
-  static extractResources(buffer: Buffer, options?: FileReadingOptions): ResourceKeyPair[] {
+  static extractResources(buffer: Buffer, options?: PackageFileReadingOptions): ResourceKeyPair[] {
     return readDbpf(buffer, options);
   }
 
   /**
-   * Reads the given buffer as a Package asynchronously, and returns a Promise
-   * that resolves with its entries in an array.
+   * Asynchronously reads the given buffer as a Package, but just returns its entries rather
+   * than a full Package object.
    * 
    * @param buffer Buffer to extract resources from
    * @param options Options for reading and cacheing the resources
    */
-  static async extractResourcesAsync(buffer: Buffer, options?: FileReadingOptions): Promise<ResourceKeyPair[]> {
+  static async extractResourcesAsync(buffer: Buffer, options?: PackageFileReadingOptions): Promise<ResourceKeyPair[]> {
     return promisify(() => Package.extractResources(buffer, options));
-  }
-
-  /**
-   * Reads the given buffer as a Package.
-   * 
-   * @param buffer Buffer to read as a package
-   * @param options Options for reading and cacheing the resources
-   */
-  static from(buffer: Buffer, options?: FileReadingOptions): Package {
-    return new Package(readDbpf(buffer, options));
-  }
-
-  /**
-   * Reads the given buffer as a Package asynchronously, and returns a Promise
-   * that resolves with it.
-   * 
-   * @param buffer Buffer to read as a package
-   * @param options Options for reading and cacheing the resources
-   */
-  static async fromAsync(buffer: Buffer, options?: FileReadingOptions): Promise<Package> {
-    return promisify(() => Package.from(buffer, options));
   }
 
   //#endregion Initialization
