51525bc93759763afe7dab6d85f1686ab07f2109
Frank Kulak <fs.kulak@gmail.com>
2022-04-26 19:24:06 -0400

    Update string-entry.test.ts

--- test/lib/resources/stbl/string-entry.test.ts
+++ test/lib/resources/stbl/string-entry.test.ts
@@ -26,13 +26,13 @@ describe("StringEntry", () => {
   describe("#key", () => {
     it("should uncache the stbl when set", () => {
       const stbl = getStbl("Normal", true);
-      expect(stbl.isCached).to.be.true;
+      expect(stbl.hasBufferCache).to.be.true;
       stbl.get(0).key++;
-      expect(stbl.isCached).to.be.false;
+      expect(stbl.hasBufferCache).to.be.false;
     });
 
     it("should update the key map when set", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       expect(stbl.getIdForKey(123)).to.be.undefined;
       const entry = stbl.add(123, "hi");
       expect(stbl.getIdForKey(123)).to.equal(0);
@@ -44,7 +44,7 @@ describe("StringEntry", () => {
 
   describe("#string", () => {
     it("should return the value of this entry", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.addAndHash("hi");
       expect(entry.value).to.equal("hi");
       expect(entry.string).to.equal("hi");
@@ -52,15 +52,15 @@ describe("StringEntry", () => {
 
     it("should uncache the stbl when set", () => {
       const stbl = getStbl("Normal", true);
-      expect(stbl.isCached).to.be.true;
+      expect(stbl.hasBufferCache).to.be.true;
       stbl.get(0).string = "new";
-      expect(stbl.isCached).to.be.false;
+      expect(stbl.hasBufferCache).to.be.false;
     });
   });
 
   describe("#value", () => {
     it("should set the value of this entry", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.addAndHash("hi");
       expect(entry.value).to.equal("hi");
       entry.value = "bye";
@@ -69,22 +69,22 @@ describe("StringEntry", () => {
 
     it("should uncache the stbl when set", () => {
       const stbl = getStbl("Normal", true);
-      expect(stbl.isCached).to.be.true;
+      expect(stbl.hasBufferCache).to.be.true;
       stbl.get(0).value = "new";
-      expect(stbl.isCached).to.be.false;
+      expect(stbl.hasBufferCache).to.be.false;
     });
   });
 
   describe("#clone()", () => {
     it("should return an entry that is equal", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       const clone = entry.clone();
       expect(entry.equals(clone)).to.be.true;
     });
 
     it("should not mutate the original", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       const clone = entry.clone();
       clone.key = 456;
@@ -92,7 +92,7 @@ describe("StringEntry", () => {
     });
 
     it("should not copy the owner", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       expect(entry.owner).to.equal(stbl);
       const clone = entry.clone();
@@ -102,28 +102,28 @@ describe("StringEntry", () => {
 
   describe("#equals()", () => {
     it("should return true when key and value are the same", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       const other = stbl.add(123, "hi");
       expect(entry.equals(other)).to.be.true;
     });
 
     it("should return false when key is different", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       const other = stbl.add(456, "hi");
       expect(entry.equals(other)).to.be.false;
     });
 
     it("should return false when value is different", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       const other = stbl.add(123, "bye");
       expect(entry.equals(other)).to.be.false;
     });
 
     it("should return false when other is undefined", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       expect(entry.equals(undefined)).to.be.false;
     });
@@ -131,13 +131,13 @@ describe("StringEntry", () => {
 
   describe("#keyEquals()", () => {
     it("should return true when key is the same", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       expect(entry.keyEquals(123)).to.be.true;
     });
 
     it("should return false when key is different", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       expect(entry.keyEquals(456)).to.be.false;
     });
@@ -146,33 +146,33 @@ describe("StringEntry", () => {
   describe("#onChange()", () => {
     it("should uncache the owning stbl", () => {
       const stbl = getStbl("Normal", true);
-      expect(stbl.isCached).to.be.true;
+      expect(stbl.hasBufferCache).to.be.true;
       stbl.get(0).onChange();
-      expect(stbl.isCached).to.be.false;
+      expect(stbl.hasBufferCache).to.be.false;
     });
   });
 
   describe("#validate()", () => {
     it("should not throw when key and string are valid", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       expect(() => entry.validate()).to.not.throw();
     });
 
     it("should throw if key is negative", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(-1, "hi");
       expect(() => entry.validate()).to.throw();
     });
 
     it("should throw if key is NaN", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(NaN, "hi");
       expect(() => entry.validate()).to.throw();
     });
 
     it("should throw if key is > 32 bit", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(0x100000000, "hi");
       expect(() => entry.validate()).to.throw();
     });
@@ -180,13 +180,13 @@ describe("StringEntry", () => {
 
   describe("#valueEquals()", () => {
     it("should return true when value is the same", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       expect(entry.valueEquals("hi")).to.be.true;
     });
 
     it("should return false when value is different", () => {
-      const stbl = StringTableResource.create();
+      const stbl = new StringTableResource();
       const entry = stbl.add(123, "hi");
       expect(entry.valueEquals("bye")).to.be.false;
     });
