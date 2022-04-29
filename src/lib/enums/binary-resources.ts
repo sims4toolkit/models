4fd7ecaf64e23590781f3fad02cedcbc358829bd
Frank Kulak <fs.kulak@gmail.com>
2022-04-28 20:15:03 -0400

    Add DdsImage to binary resource types.

--- src/lib/enums/binary-resources.ts
+++ src/lib/enums/binary-resources.ts
@@ -1,8 +1,11 @@
 /**
- * Types for binary resources (i.e. any resource that follows a BT).
+ * Types for binary resources (i.e. any resource that follows a BT). Note that
+ * this enum is intentionally incomplete, as other binary resources are not yet
+ * supported by S4TK.
  */
 enum BinaryResourceType {
   CombinedTuning = 0x62E94D38,
+  DdsImage = 0xB6C8B6A0,
   DstImage = 0x00B2D882,
   SimData = 0x545AC67A,
   StringTable = 0x220557DA,
