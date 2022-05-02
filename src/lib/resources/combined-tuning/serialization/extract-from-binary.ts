b3c16669a44735c2b36c2dbad4954b41000cc74a
Frank Kulak <fs.kulak@gmail.com>
2022-05-02 11:51:12 -0400

    Update extract-from-binary.ts

--- src/lib/resources/combined-tuning/serialization/extract-from-binary.ts
+++ src/lib/resources/combined-tuning/serialization/extract-from-binary.ts
@@ -3,6 +3,30 @@ import { XmlExtractionOptions } from "../../../common/options";
 import { BinaryDataResourceDto } from "../../abstracts/data-resource";
 import XmlResource from "../../xml/xml-resource";
 
+/*
+' SCUMBUMBO'S NOTES FROM XML EXTRACTOR:
+'
+' Table 0 stores the basic info for the packed xml document
+' Table 1 stores a list of nodes
+'    - nodes prior to the the first_element are text
+'    - the remaining nodes are xml tags
+'    - mValue(0) = offset into child nodes (table 3)
+'    - mValue(1) = row number to get text (via table 5)
+'    - mValue(2) = offset into attribute nodes (table 4)
+' Table 2 stores a list of row numbers storing attribute value,name pairs (via table 5)
+' Table 3 stores a list of child nodes
+'    - these are offsets back into table 1
+'    - a RELOFFSET_NULL offset indicates the end of this node's children
+' Table 4 stores a list of attributes
+'    - these are offsets into table 2
+'    - a RELOFFSET_NULL offset indicates the end of this node's attributes
+' Table 5 stores a list of offsets pointing to the actual string data (table 6)
+' Table 6 stores a list of null terminated strings
+'
+' Offsets are not calculated from a fixed position, but are offsets
+' from the current file position.
+*/
+
 /**
  * TODO:
  * 
@@ -15,12 +39,22 @@ export default function extractTuningFromCombinedBinary(
   buffer: Buffer,
   options?: XmlExtractionOptions // TODO: actually use these
 ): XmlResource[] {
-  // TODO:
+  const [
+    metaDataTable,         // meta data
+    nodeObjectsTable,      // actual node objects
+    attributeObjectsTable, // actual attributes data
+    nodeRefsTable,         // offsets to node objects (for listing)
+    attributeRefsTable,    // offsets to attribute objects (for listing)
+    stringRefsTable,       // offsets to null-terminated strings (for listing)
+    stringObjectsTable     // actual null-terminated strings
+  ] = binaryModel.mTableData;
 
   const {
-    first_element,
-    top_element,
-    element_count,
-    string_table
-  } = binaryModel.mTableData[0].mRow[0];
+    first_element, // index in table 1 (nodeObjectsTable)
+    top_element,   // ?
+    element_count, // ?
+    string_table   // ?
+  } = metaDataTable.mRow[0];
+
+
 }
