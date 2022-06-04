c1ddab81d3a28818cb3e8167de8b5f94ebc3c7fb
Frank Kulak <fs.kulak@gmail.com>
2022-06-04 14:19:24 -0400

    Add StringTableLocale.getLocaleCode()

--- src/lib/enums/stbl-locales.ts
+++ src/lib/enums/stbl-locales.ts
@@ -59,6 +59,36 @@ namespace StringTableLocale {
   export function getInstanceBase(instance: bigint): bigint {
     return instance & 0xFFFFFFFFFFFFFFn;
   }
+
+  /**
+   * Returns the 5-letter code that is appended to string table packages that
+   * are shipped with the game. Returns undefined if locale is not recognized.
+   * 
+   * @param locale Locale to get code for
+   */
+  export function getLocaleCode(locale: StringTableLocale): string {
+    switch (locale) {
+      case StringTableLocale.English: return "ENG_US";
+      case StringTableLocale.ChineseSimplified: return "CHS_CN";
+      case StringTableLocale.ChineseTraditional: return "CHT_CN";
+      case StringTableLocale.Czech: return "CZE_CZ";
+      case StringTableLocale.Danish: return "DAN_DK";
+      case StringTableLocale.Dutch: return "DUT_NL";
+      case StringTableLocale.Finnish: return "FIN_FI";
+      case StringTableLocale.French: return "FRE_FR";
+      case StringTableLocale.German: return "GER_DE";
+      case StringTableLocale.Italian: return "ITA_IT";
+      case StringTableLocale.Japanese: return "JPN_JP";
+      case StringTableLocale.Korean: return "KOR_KR";
+      case StringTableLocale.Norwegian: return "NOR_NO";
+      case StringTableLocale.Polish: return "POL_PL";
+      case StringTableLocale.Portuguese: return "POR_BR";
+      case StringTableLocale.Russian: return "RUS_RU";
+      case StringTableLocale.Spanish: return "SPA_EA";
+      case StringTableLocale.Swedish: return "SWE_SE";
+      default: return undefined;
+    }
+  }
 }
 
 // `export default enum` not supported by TS
