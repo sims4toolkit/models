e6b662b84a63b36ee4758f673d578a38f2e6b40a
Frank Kulak <fs.kulak@gmail.com>
2022-06-03 21:39:44 -0400

    Add some enum functions.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased] - TBD
 ### Added
 - Add `limit` to PackageFileReadingOptions.
-- Add `all()` to TuningResourceType and SimDataGroup.
+- Add `all()` to TuningResourceType, SimDataGroup, and BinaryResourceType.
+- Add `parseAttr()` and `getAttr()` to TuningResourceType.
 
 ## [0.4.1] - 2022/06/01
 ### Added
--- src/lib/enums/binary-resources.ts
+++ src/lib/enums/binary-resources.ts
@@ -1,3 +1,5 @@
+import { getAllEnumValues } from "../common/helpers";
+
 /**
  * Types for binary resources (i.e. any resource that follows a BT). Note that
  * this enum is intentionally incomplete, as other binary resources are not yet
@@ -9,6 +11,15 @@ enum BinaryResourceType {
   DstImage = 0x00B2D882,
   SimData = 0x545AC67A,
   StringTable = 0x220557DA,
+}
+
+namespace BinaryResourceType {
+  /**
+   * Returns an array of all binary resource types.
+   */
+  export function all(): BinaryResourceType[] {
+    return getAllEnumValues(BinaryResourceType);
+  }
 }
 
 // `export default enum` not supported by TS
